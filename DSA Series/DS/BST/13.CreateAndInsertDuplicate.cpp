6792a530a802b95b0210f5055ec3ac44e3074824
asimsiddiqui1 <>
2023-07-18 08:05:07 +0000

    Added code in BST

---
+++ DSA Series/DS/BST/13.CreateAndInsertDuplicate.cpp
@@ -0,0 +1,109 @@
+/*
+Create & Insert Duplicate Node
+Send Feedback
+For a given a Binary Tree of type integer, duplicate every node of the tree and attach it to the left of itself.
+The root will remain the same. So you just need to insert nodes in the given Binary Tree.
+Example:
+alt txt
+
+After making the changes to the above-depicted tree, the updated tree will look like this.
+alt txt
+
+You can see that every node in the input tree has been duplicated and inserted to the left of itself.
+Input format :
+The first and the only line of input will contain the node data, all separated by a single space. Since -1 is used as an indication whether the left or right node data exist for root, it will not be a part of the node data.
+Output Format :
+The updated tree will be printed in a level order fashion where each level will be printed on a new line. 
+Elements on every level will be printed in a linear fashion. A single space will separate them.
+ Note:
+You are not required to print anything explicitly. It has already been taken care of. Just implement the function to achieve the desired structure of the tree.
+Hint:
+First, store the left node. Next, insert a duplicate node to the left of the current node. Then, call the function for the stored left node, which will return a modified node. Attach this modified node to the left of the duplicate node. Finally, proceed to call the function for the right node of the root
+Constraints :
+1 <= N <= 10^5
+Where N is the total number of nodes in the binary tree.
+
+Time Limit: 1 sec
+Sample Input 1:
+10 20 30 40 50 -1 60 -1 -1 -1 -1 -1 -1
+Sample Output 1:
+10 
+10 30 
+20 30 60 
+20 50 60 
+40 50 
+40 
+Sample Input 2:
+8 5 10 2 6 -1 -1 -1 -1 -1 7 -1 -1
+Sample Output 2:
+8 
+8 10 
+5 10 
+5 6 
+2 6 7 
+2 7
+*/
+
+/************************************************************
+
+    Following is the Binary Tree node structure
+
+    template <typename T>
+    class BinaryTreeNode
+    {
+        public :
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+
+        BinaryTreeNode(T data)
+        {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+************************************************************/
+/*
+APPROACH
+1.CASE - IF ROOT IS HAVING LEFT CHILD
+2.CREATE A DUPLICATE NODE WITH ROOTDATA , ATTACH DUPLICATE NODE AS LEFTCHILD WITH HELP OF TEMPNODE
+3.OTHERWISE
+4.CASE - If root is not having leftChild and having rightChild
+5.Create a duplicateNode with rootData, Attach duplicateNode as root's leftChild
+6.DO THIS FOR LST(root->left->left) AND RST(root->right)
+*/
+
+void insertDuplicateNode(BinaryTreeNode<int> *root) {
+    // Write your code here
+   // if tree is empty then return 
+    if(root == NULL){
+        return;
+    }
+
+    // If root is having leftChild then creating a duplicateNode to store root Data .
+    // Now create tempNode to store root's leftChild so that rootNode and its leftChild connection
+    // doesn't get disconnected.
+    //Attach duplicateNode as root's leftChild then to duplicateNode attach root's leftChild store in tempNode
+    if(root->left != NULL){
+         BinaryTreeNode<int>* duplicate = new BinaryTreeNode<int>(root->data);
+        BinaryTreeNode<int>* temp = root->left;
+        root->left = duplicate;
+        duplicate->left = temp;
+    }
+    // Case : If root is not having leftChild and having rightChild
+    // Currently at root's and it is having rightChild, create a duplicateNode with rootData
+    //  Attach duplicateNode as root's leftChild
+    else{
+       BinaryTreeNode<int>* duplicate = new BinaryTreeNode<int>(root->data);
+        root->left = duplicate;
+    }
+    //Inserting Duplicate in LST
+    //As we are attaching duplicate node as leftChild so nextNode for which Duplicate is made
+    //will be root's left of left
+    insertDuplicateNode(root->left->left);
+    //inserting duplicate in RST
+    insertDuplicateNode(root->right);
+}
+
